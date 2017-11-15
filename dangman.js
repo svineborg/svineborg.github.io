b0bc19e86557122686e1e6c5b40854ea2e0b28ca
svineborg <svineborg@gmail.com>
2017-11-14 21:17:49 -0800

    initial commit

--- dangman.js
+++ dangman.js
@@ -1,1 +1,13 @@
-document.getElementByYid('demo').innerHTML = "ya clicked me oh man"
+var the_text = "potato poo poo"
+function changeTheWord(a_new_word){
+	the_text = a_new_word
+}
+
+function checkGuess(a_guess){
+	acceptable = 'False'
+	if(a_guess.length == 1 && a_guess.match(/[a-a]/i)){
+		acceptable = 'True'
+	}
+	return acceptable
+}
+
