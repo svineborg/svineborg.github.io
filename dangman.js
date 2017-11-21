f2b4829b561b3d6784b9cf8588705691ebf2e260
svineborg <svineborg@gmail.com>
2017-11-20 23:37:42 -0800

    a new working version

--- dangman.js
+++ dangman.js
@@ -1,42 +1,73 @@
-var the_word = "potato poo poo"
-function changeTheWord(a_new_word){
-	the_text = a_new_word
+var word = ''
+var guesses = new Array()
+var lives = 0
+var message = ''
+
+function displayWord(){
+	//display the word to be guessed, blotting out characters not yet guessed
+	shownWord = ''
+	for(x=0;x<word.length;x++){
+		if(guesses.indexOf(word[x].toLowerCase()) != -1 || /[a-z]/i.test(word[x]) == false){
+			shownWord += word[x]
+		} else {
+			shownWord += "_"
+		}
+	}
+	return shownWord
+}
+
+function refresh(){
+	//refresh the elements of the game
+	document.getElementById('lives').innerHTML = "Lives left:" + lives
+	document.getElementById('message').innerHTML = message
+	document.getElementById('guesses').innerHTML = "Guesses:" + guesses
+	document.getElementById('word').innerHTML = displayWord()
 }
 
 function reset(){
-	reset_word = the_word.replace(/[a-z]/gi,'_')
-	document.getElementById("the_text").innerHTML = reset_word
+	// set a new game, static values
+	word = "pooPoo okay, how about that???"
+	lives = 10
+	guesses = new Array()
+	message = "ALRIGHT! New game!"
+	refresh()
+	console.log('reset')
 }
+
 function checkGuess(a_guess){
-	acceptable = 'False'
-	if(a_guess.length == 1 && a_guess.match(/[a-z]/i)){
-		acceptable = 'True'
-	}
-	return acceptable
-}
-
-function updateWord(a_char){
-	current_word = document.getElementById("the_text").innerHTML
-	new_word = ''
-	for(x=0;x<current_word.length;x++){
-		if(a_char == the_word[x]){
-			new_word += a_char
-		} else {
-			new_word += current_word[x]
-		}
-		console.log(new_word[x])
-	}
-	console.log(new_word)
-	document.getElementById("the_text").innerHTML = new_word
-}
-
-function submitGuess(){
-	var a_new_guess = document.getElementById("guess").value
-	if(checkGuess(a_new_guess) == 'False'){
-		console.log("no, only single letters")
-		return "false"
+	acceptableGuess = false
+	if(a_guess.length != 1){
+		message = "NO! One character only."
+	} else if ( /[a-z]/i.test(a_guess) == false) {
+		message = "NO! Only the alphabets, poopoohead!"
 	} else {
-		console.log("good guess! :" + a_new_guess)
-		updateWord(a_new_guess)
+		message = "Good guess :)"
+		acceptableGuess = true
 	}
+	return acceptableGuess
+}
+
+function loseLife(amt){
+	lives -= amt
+	stillAlive = true
+	if(lives < 1){
+		console.log('game is over')
+		stillAlive = false
+	}
+	return stillAlive
+}
+
+function submit(a_guess){
+	if(checkGuess(a_guess)){
+	//is the guess an acceptable format?
+		if(guesses.indexOf(a_guess.toLowerCase()) != -1){
+			message += "You already guessed " + a_guess + " ya dingus!"
+		} else {
+			guesses.push(a_guess.toLowerCase())
+			if(word.indexOf(a_guess) == -1){
+				loseLife(1)
+			}
+		}
+	}
+	refresh()
 }
